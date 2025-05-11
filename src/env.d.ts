acd2fba27baacc6bae6b61c0b84403194d3e2daf
Iván Sánchez <ivan.ravecca@gmail.com>
2025-05-11 12:50:13 -0300

    Add extra variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
+  readonly VITE_SITE_URL: string;
 }
 
 interface ImportMeta {
--- src/services/DiplomaService.tsx
+++ src/services/DiplomaService.tsx
@@ -1,4 +1,5 @@
 const VITE_API_URL = import.meta.env.VITE_API_URL;
+const VITE_SITE_URL = import.meta.env.VITE_SITE_URL;
 
 export const getDiploma = async (diplomaID: string) => {
   return await fetch(`${VITE_API_URL}/diploma/${diplomaID}`, {
@@ -7,7 +8,7 @@ export const getDiploma = async (diplomaID: string) => {
 };
 
 export const generateDiploma = async (driveURL: string) => {
-  return await fetch(`${VITE_API_URL}/diploma/generate?link=${driveURL}`, {
+  return await fetch(`${VITE_SITE_URL}/diploma/generate?link=${driveURL}`, {
     method: "GET",
   });
 };
